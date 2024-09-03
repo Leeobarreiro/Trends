a1e6fc10db5805fce4a02425d093e26a4d3cd122
Leonardo Gomes Barreiro <Leo.barreiro@hotmail.com>
2024-09-03 17:20:57 -0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,64 +1,67 @@
-const MONGO_URI = 'mongodb+srv://Rivick:Xdplos2adr@cluster0.mongodb.net/sample_mflix?retryWrites=true&w=majority';
-const MONGO_DATA_API_URL = 'https://sa-east-1.aws.data.mongodb-api.com/app/data-jlaekkp/endpoint/data/v1';
-const MONGO_API_KEY = 'yJjLHkVWTUCMMhyGj1tWVJJrrpCig1azv4lguI7SMwnmLIH3XJvvXibWxS6ivwJs';
+import { MongoClient } from "npm:mongodb"
 
-// Função para conectar ao MongoDB e buscar dados
-export default async function handler(req: Request) {
-  if (req.method === 'GET') {
-    // Definindo o corpo da requisição para a API do MongoDB
-    const body = {
-      dataSource: 'Cluster0',
-      database: 'sample_mflix',
-      collection: 'comments',
-	  
-      pipeline: [
-        {
-          $group: {
-            _id: "$hashtag",
-            count: { $sum: 1 },
-          },
-        },
-        {
-          $project: {
-            _id: 0,
-            hashtag: "$_id",
-            count: 1,
-          },
-        },
-        {
-          $sort: { count: -1 },
-        },
-        {
-          $limit: 10, // Você pode alterar conforme necessário
-        },
-      ],
-    };
+const MONGO_URI = Deno.env.get("MONGO_URI") as string
 
-    try {
-      const response = await fetch(MONGO_DATA_API_URL + '/action/aggregate', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'api-key': MONGO_API_KEY,
-        },
-        body: JSON.stringify(body),
-      });
+const client = new MongoClient(MONGO_URI)
 
-      const data = await response.json();
+const db = client.db("bsky")
 
-      if (response.ok) {
-        return new Response(JSON.stringify(data.documents), {
-          headers: { 'Content-Type': 'application/json' },
-        });
-      } else {
-        console.error('Erro na resposta da API MongoDB:', data);
-        return new Response('Erro ao buscar dados', { status: 500 });
-      }
-    } catch (error) {
-      console.error('Erro ao conectar à MongoDB Data API:', error);
-      return new Response('Erro interno do servidor', { status: 500 });
-    }
-  } else {
-    return new Response('Método não permitido', { status: 405 });
-  }
+const collection = db.collection("hashtags")
+
+const hashtags = async ({ limit }: { limit: number } = { limit: 25 }) => {
+	const pipeline = [
+		{
+			$group: {
+				_id: "$hashtag",
+				count: { $sum: 1 },
+			},
+		},
+		{
+			$project: {
+				_id: 0,
+				hashtag: "$_id",
+				count: 1,
+			},
+		},
+		{
+			$sort: { count: -1 },
+		},
+		{
+			$limit: limit,
+		},
+	]
+
+	return await collection
+		.aggregate(pipeline)
+		.toArray()
 }
+
+const handler = async (req: Request): Promise<Response> => {
+	const url = new URL(req.url)
+
+	const limit = Number(url.searchParams.get("limit") || 25)
+
+	const headers = new Headers({
+		"Content-Type": "application/json",
+		"Access-Control-Allow-Origin": "*",
+		"Access-Control-Allow-Methods": "GET, OPTIONS",
+		"Access-Control-Allow-Headers":
+			"Origin, X-Requested-With, Content-Type, Accept",
+	})
+
+	const data = await hashtags({ limit })
+
+	if (!data) {
+		return new Response(null, {
+			status: 400,
+		})
+
+	}
+
+	return new Response(JSON.stringify(data), {
+		status: 200,
+		headers,
+	})
+}
+
+Deno.serve({ port: 80 }, handler)
