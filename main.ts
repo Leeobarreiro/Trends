2f3ba227034eb662f006d8efff86c4b77679077a
Leonardo Gomes Barreiro <Leo.barreiro@hotmail.com>
2024-09-03 16:35:18 -0300

    a

--- main.ts
+++ main.ts
@@ -1,69 +1,63 @@
-export const config = {
-	runtime: 'edge', // Utilizando o runtime edge para serverless
-  };
-  
-  export default async function handler(req: Request) {
-	if (req.method === 'GET') {
-	  const MONGO_DATA_API_URL = Deno.env.get('MONGO_DATA_API_URL');
-	  const MONGO_API_KEY = Deno.env.get('MONGO_API_KEY');
-  
-	  if (!MONGO_DATA_API_URL || !MONGO_API_KEY) {
-		return new Response('Configuração da API MongoDB faltando', { status: 500 });
-	  }
-  
-	  const body = {
-		dataSource: 'Cluster0', // Nome do cluster no MongoDB Atlas
-		database: 'sample_mflix', // Nome do banco de dados
-		collection: 'comments', // Nome da coleção
-		pipeline: [
-		  {
-			$group: {
-			  _id: "$hashtag",
-			  count: { $sum: 1 }
-			}
-		  },
-		  {
-			$project: {
-			  _id: 0,
-			  hashtag: "$_id",
-			  count: 1
-			}
-		  },
-		  {
-			$sort: { count: -1 }
-		  },
-		  {
-			$limit: 10
-		  }
-		]
-	  };
-  
-	  try {
-		const response = await fetch(`${MONGO_DATA_API_URL}/action/aggregate`, {
-		  method: 'POST',
-		  headers: {
-			'Content-Type': 'application/json',
-			'api-key': MONGO_API_KEY,
-		  },
-		  body: JSON.stringify(body),
-		});
-  
-		const data = await response.json();
-  
-		if (response.ok) {
-		  return new Response(JSON.stringify(data.documents), {
-			headers: { 'Content-Type': 'application/json' },
-		  });
-		} else {
-		  console.error('Erro na resposta da API MongoDB:', data);
-		  return new Response('Erro ao buscar dados', { status: 500 });
-		}
-	  } catch (error) {
-		console.error('Erro ao conectar à MongoDB Data API:', error);
-		return new Response('Erro interno do servidor', { status: 500 });
-	  }
-	} else {
-	  return new Response('Método não permitido', { status: 405 });
-	}
+
+const MONGO_URI = 'mongodb+srv://Rivick:Xdplos2adr@cluster0.mongodb.net/sample_mflix?retryWrites=true&w=majority';
+const MONGO_DATA_API_URL = 'https://sa-east-1.aws.data.mongodb-api.com/app/data-jlaekkp/endpoint/data/v1';
+const MONGO_API_KEY = 'yJjLHkVWTUCMMhyGj1tWVJJrrpCig1azv4lguI7SMwnmLIH3XJvvXibWxS6ivwJs';
+
+// Conectar ao MongoDB e buscar dados
+export default async function handler(req: Request) {
+  if (req.method === 'GET') {
+    const body = {
+      dataSource: 'Cluster0',
+      database: 'sample_mflix',
+      collection: 'comments',
+      pipeline: [
+        {
+          $group: {
+            _id: "$hashtag",
+            count: { $sum: 1 }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            hashtag: "$_id",
+            count: 1
+          }
+        },
+        {
+          $sort: { count: -1 }
+        },
+        {
+          $limit: 10
+        }
+      ]
+    };
+
+    try {
+      const response = await fetch(MONGO_DATA_API_URL + '/action/aggregate', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'api-key': MONGO_API_KEY,
+        },
+        body: JSON.stringify(body)
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        return new Response(JSON.stringify(data.documents), {
+          headers: { 'Content-Type': 'application/json' },
+        });
+      } else {
+        console.error('Erro na resposta da API MongoDB:', data);
+        return new Response('Erro ao buscar dados', { status: 500 });
+      }
+    } catch (error) {
+      console.error('Erro ao conectar à MongoDB Data API:', error);
+      return new Response('Erro interno do servidor', { status: 500 });
+    }
+  } else {
+    return new Response('Método não permitido', { status: 405 });
   }
-  
+}
--- watcher.ts
+++ watcher.ts
@@ -1,91 +1,89 @@
-import { cborDecodeMulti } from "npm:@atproto/common";
-import { CarReader } from "npm:@ipld/car/reader";
-import { decode } from "npm:@ipld/dag-cbor";
-import { MongoClient } from "npm:mongodb";
+import { cborDecodeMulti } from "npm:@atproto/common"
+import { CarReader } from "npm:@ipld/car/reader"
+import { decode } from "npm:@ipld/dag-cbor"
+import { MongoClient } from "npm:mongodb"
 
-// Defina diretamente a URL de conexão do MongoDB
-const MONGO_URI = 'mongodb+srv://Rivick:Xdplos2adr@cluster0.mongodb.net/sample_mflix?retryWrites=true&w=majority';
-const MONGO_DATA_API_URL = 'https://sa-east-1.aws.data.mongodb-api.com/app/data-jlaekkp/endpoint/data/v1';
-const MONGO_API_KEY = 'yJjLHkVWTUCMMhyGj1tWVJJrrpCig1azv4lguI7SMwnmLIH3XJvvXibWxS6ivwJs';
-const BATCH_SIZE = 10;
-const BSKY_FIREHOSE_URL = "wss://bsky.network/xrpc/com.atproto.sync.subscribeRepos";
+const MONGO_URI = Deno.env.get("MONGO_URI") as string
+const BATCH_SIZE = 10
+const BSKY_FIREHOSE_URL = "wss://bsky.network/xrpc/com.atproto.sync.subscribeRepos"
 
-const client = new MongoClient(MONGO_URI);
-const db = client.db("sample_mflix");
-const collection = db.collection("comments");
+const client = new MongoClient(MONGO_URI)
+const db = client.db("bsky")
+const collection = db.collection("hashtags")
 
-const messages: { hashtag: string, path: string, createdAt: Date }[] = [];
+const messages: { hashtag: string, path: string, createdAt: Date }[] = []
 
 const commit = async () => {
-  if (messages.length <= 10) return;
+    if (messages.length <= 10) return
 
-  const toInsert = messages.splice(0, BATCH_SIZE);
+    const toInsert = messages.splice(0, BATCH_SIZE)
 
-  const result = await collection.insertMany(toInsert);
+    const result = await collection.insertMany(toInsert)
 
-  if (result.acknowledged) {
-    console.log(`Inserted`, toInsert.map(({ hashtag }) => `#${hashtag}`).join(' '), `(${result.insertedCount})`);
-  } else {
-    console.error(result);
-    messages.push(...toInsert);
-  }
-};
+    if (result.acknowledged) {
+        console.log(`Inserted`, toInsert.map(({ hashtag }) => `#${hashtag}`).join(' '), `(${result.insertedCount})`)
+    }
+    else {
+        console.error(result)
+        messages.push(...toInsert)
+    }
+}
 
 const onMessage = async ({ data }: MessageEvent<ArrayBuffer>) => {
-  const [header, payload] = cborDecodeMulti(new Uint8Array(data)) as any;
-  if (header.op === 1 && header.t === "#commit" && payload) {
-    for (const op of payload.ops) {
-      if (op.action == "create") {
-        const cr = await CarReader.fromBytes(payload.blocks);
-        if (op.cid) {
-          const block = await cr.get(op.cid as any);
-          if (block) {
-            const message = decode(block.bytes) as any;
-            if (
-              message.$type === "app.bsky.feed.post" &&
-              message.text &&
-              message?.facets
-            ) {
-              for (const facet of message.facets) {
-                for (const feature of facet?.features) {
-                  if (feature.tag) {
-                    messages.push({
-                      hashtag: feature.tag,
-                      path: op.path.toString(),
-                      createdAt: new Date(message.createdAt),
-                    });
-                  }
-                  if (messages.length >= BATCH_SIZE) await commit();
+    const [header, payload] = cborDecodeMulti(new Uint8Array(data)) as any
+    if (header.op === 1 && header.t === "#commit" && payload) {
+        for (const op of payload.ops) {
+            if (op.action == "create") {
+                const cr = await CarReader.fromBytes(payload.blocks)
+                if (op.cid) {
+                    const block = await cr.get(op.cid as any)
+                    if (block) {
+                        const message = decode(block.bytes) as any
+                        if (
+                            message.$type === "app.bsky.feed.post" &&
+                            message.text &&
+                            message?.facets
+                        ) {
+                            for (const facet of message.facets) {
+                                for (const feature of facet?.features) {
+                                    if (feature.tag) {
+                                        messages.push({
+                                            hashtag: feature.tag,
+                                            path: op.path.toString(),
+                                            createdAt: new Date(message.createdAt)
+                                        })
+                                    }
+                                    if (messages.length >= BATCH_SIZE) await commit()
+                                }
+                            }
+                        }
+                    }
                 }
-              }
             }
-          }
         }
-      }
     }
-  }
-};
+}
 
-let ws: WebSocket | null;
+let ws: WebSocket | null
 
 const onError = (error: Event | ErrorEvent) => {
-  console.error(error);
-  if (ws) ws.close();
-};
+    console.error(error)
+    if (ws) ws.close()
+}
 
 const onClose = () => {
-  console.log('Connection closed');
-  ws = null;
-  setTimeout(init, 5000);
-};
+    console.log('Connection closed')
+    ws = null
+    setTimeout(init, 5000)
+}
 
 const init = () => {
-  ws = new WebSocket(BSKY_FIREHOSE_URL);
-  ws.binaryType = 'arraybuffer';
-  ws.onmessage = onMessage;
-  ws.onerror = onError;
-  ws.onclose = onClose;
-  ws.onopen = () => console.log('Connection opened');
-};
+    ws = new WebSocket(BSKY_FIREHOSE_URL)
+    ws.binaryType = 'arraybuffer'
+    ws.onmessage = onMessage
+    ws.onerror = onError
+    ws.onclose = onClose
+    ws.onopen = () => console.log('Connection opened')
+}
 
-init();
+init()
